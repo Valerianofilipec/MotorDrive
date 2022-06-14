827caedf350bb9f9e828c68ec95ccbc0370dbefe
Valeriano Filipe <valeriofilipec@gmail.com>
2022-06-14 23:13:51 +0100

    modulejs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const cors = require("cors");
-const express = require("express");
-const router = require("./router");
+import cors from "cors";
+//const cors = require("cors");
+import express from "express";
+import {routes} from "./routes/index.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(router);// importar as respectivas rotas
+app.use(routes);// importar as respectivas rotas
 
 app.listen(3333, console.log(" MotorDrive's Server running!"));
